bd1f358a2a697e311ad1202fa304b56e7ea68821
RemZiQ <remziq1992@gmail.com>
2019-02-26 00:52:16 +0300

    feat: add build Mentor_dashboard.

---
+++ docs/precache-manifest.e198691315ce88cf9fc09f78220a36a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "1dd37d6642645ae5d2bf",
+    "url": "./static/js/main.1dd37d66.chunk.js"
+  },
+  {
+    "revision": "02deb9d3c8ab9464468a",
+    "url": "./static/js/2.02deb9d3.chunk.js"
+  },
+  {
+    "revision": "1dd37d6642645ae5d2bf",
+    "url": "./static/css/main.284b96ab.chunk.css"
+  },
+  {
+    "revision": "835f7582373b6d4374d98005c1128018",
+    "url": "./index.html"
+  }
+];
