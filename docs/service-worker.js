8a35752c4b1b3eda43576c34aa7ae7f0c9c34100
RemZiQ <remziq1992@gmail.com>
2019-02-26 01:34:07 +0300

    feat: add Mentor_dashboard.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3dace42acae7dd9b1de2c59c14e3ea6.js"
+  "./precache-manifest.e198691315ce88cf9fc09f78220a36a9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
